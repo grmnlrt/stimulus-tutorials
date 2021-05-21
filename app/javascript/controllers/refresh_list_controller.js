9b99e3296c34f2611cc2990ced7193a0249b786c
germain loret <germain.loret@gmail.com>
2021-05-21 16:24:19 +0200

    Refacto

--- app/javascript/controllers/refresh_list_controller.js
+++ app/javascript/controllers/refresh_list_controller.js
@@ -8,7 +8,6 @@ export default class extends Controller {
     fetch(url, { headers: { 'Accept': 'text/plain' } })
       .then(response => response.text())
       .then((data) => {
-        console.log(data)
         this.listTarget.outerHTML = data;
       })
   }
