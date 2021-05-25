2f7888a0b48fc3d395ee406501a37b02064e4cfd
germain loret <germain.loret@gmail.com>
2021-05-25 19:30:33 +0200

    Add a limit value to character-counter controller

--- app/javascript/controllers/character_counter_controller.js
+++ app/javascript/controllers/character_counter_controller.js
@@ -2,11 +2,14 @@ import { Controller } from "stimulus"
 
 export default class extends Controller {
   static targets = ["counter"]
+  static values = { limit: Number }
 
   updateCounter(event) {
     const numberOfCharacters = event.currentTarget.value.length;
-    if (numberOfCharacters > 0) {
-      this.counterTarget.innerHTML = `${numberOfCharacters} characters`;
+    if (numberOfCharacters > this.limitValue) {
+      this.counterTarget.innerHTML = `Number of charater exceeded by ${numberOfCharacters - this.limitValue}`;
+    } else if (numberOfCharacters > 0) {
+      this.counterTarget.innerHTML = `${this.limitValue - numberOfCharacters} characters left`;
     } else {
       this.counterTarget.innerHTML = "";
     }
